e53101cf52ae2c1ec5f8c045961bdcaa0a5768ca
Tomachi (ICHIGO) <lego@aol.jp>
2020-02-29 12:45:20 +0900

    add: 10000以上の移動距離テレポートを禁止するように

---
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_AntiProblemTeleport.java
@@ -0,0 +1,27 @@
+package com.jaoafa.MyMaid3.Event;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
+
+public class Event_AntiProblemTeleport extends MyMaidLibrary implements Listener {
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerTeleportEvent(PlayerTeleportEvent event) {
+		Location from = event.getFrom();
+		Location to = event.getTo();
+		Player player = event.getPlayer();
+		if (!from.getWorld().getName().equals(to.getWorld().getName())) {
+			return;
+		}
+		if (from.distance(to) >= 10000) {
+			player.sendMessage("[TeleportCheck] " + ChatColor.GREEN + "テレポートに失敗しました。");
+			event.setCancelled(true);
+		}
+	}
+}
