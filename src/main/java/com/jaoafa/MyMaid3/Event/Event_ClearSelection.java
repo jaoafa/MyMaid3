1962d1d6304a0a0f307566729f0863a022422550
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-25 01:32:13 +0900

    add: 走りながら空気右クリックで//selコマンドを実行するように

---
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_ClearSelection.java
@@ -0,0 +1,50 @@
+package com.jaoafa.MyMaid3.Event;
+
+import java.util.LinkedList;
+import java.util.UUID;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
+
+import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
+
+public class Event_ClearSelection extends MyMaidLibrary implements Listener {
+	LinkedList<UUID> sprinting = new LinkedList<>();
+
+	@EventHandler
+	public void onAirClick(PlayerInteractEvent event) {
+		if (event.getAction() != Action.LEFT_CLICK_AIR)
+			return; // 空気左クリック以外無視
+
+		Player player = event.getPlayer();
+		if (!sprinting.contains(player.getUniqueId())) {
+			return;
+		}
+
+		if (player.getGameMode() != GameMode.CREATIVE) {
+			return;
+		}
+
+		boolean bool = player.performCommand("//sel");
+		if (!bool) {
+			player.sendMessage("[SEL] " + ChatColor.GREEN + "//selコマンドの実行に失敗しました。");
+		}
+		event.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onToggleSprint(PlayerToggleSprintEvent event) {
+		Player player = event.getPlayer();
+		if (event.isSprinting()) {
+			sprinting.add(player.getUniqueId());
+		} else {
+			sprinting.remove(player.getUniqueId());
+		}
+	}
+}
