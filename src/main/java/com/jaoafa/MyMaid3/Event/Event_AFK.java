507b8e64a93b0790ec727d3d0f28a62076d60f37
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-24 10:07:20 +0900

    add: No.36の実績追加

--- src/main/java/com/jaoafa/MyMaid3/Event/Event_AFK.java
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_AFK.java
@@ -10,6 +10,9 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import com.connorlinfoot.titleapi.TitleAPI;
 import com.jaoafa.MyMaid3.Lib.AFKPlayer;
 import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
+import com.jaoafa.jaoSuperAchievement2.API.AchievementAPI;
+import com.jaoafa.jaoSuperAchievement2.API.Achievementjao;
+import com.jaoafa.jaoSuperAchievement2.Lib.AchievementType;
 
 public class Event_AFK extends MyMaidLibrary implements Listener {
 	@EventHandler
@@ -27,6 +30,12 @@ public class Event_AFK extends MyMaidLibrary implements Listener {
 	public void OnEvent_PlayerQuit(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
 		AFKPlayer afkplayer = new AFKPlayer(player);
+		if (afkplayer.isAFK()) {
+			if (!Achievementjao.getAchievement(player, new AchievementType(36))) {
+				player.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
+				return;
+			}
+		}
 		afkplayer.clear();
 	}
 
