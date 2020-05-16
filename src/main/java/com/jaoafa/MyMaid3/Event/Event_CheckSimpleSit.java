7c6fbf436ddd8b759da792dea8e3f595a9d76217
Tomachi (ICHIGO) <lego@aol.jp>
2020-05-16 23:35:30 +0900

    change: SimpleSitプラグインのsit, layコマンドを使用した迷惑行為の規制追加

---
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_CheckSimpleSit.java
@@ -0,0 +1,35 @@
+package com.jaoafa.MyMaid3.Event;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
+
+public class Event_CheckSimpleSit extends MyMaidLibrary implements Listener {
+	@EventHandler
+	public void onSimpleSitPluginCommand(PlayerCommandPreprocessEvent event) {
+		String command = event.getMessage();
+		Player player = event.getPlayer();
+		String[] args = command.split(" ", 0);
+		if (args.length == 0) {
+			return; // 本来発生しないと思うけど
+		}
+
+		if (!args[0].equalsIgnoreCase("/sit")
+				&& !args[0].equalsIgnoreCase("/simplesit:sit")
+				&& !args[0].equalsIgnoreCase("/lay")
+				&& !args[0].equalsIgnoreCase("/simplesit:lay")) {
+			return; // sit, layコマンド以外
+		}
+
+		if (player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
+			player.sendMessage("[SimpleSit] " + ChatColor.GREEN
+					+ "このコマンドはサバイバルモード・アドベンチャーモードの時には利用できません。クリエイティブモードに切り替えてから実行してください。");
+			return;
+		}
+	}
+}
