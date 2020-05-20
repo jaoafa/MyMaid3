7df72b4dd07ec2947b199b6bf9e4408c492ed41d
Tomachi (ICHIGO) <lego@aol.jp>
2020-05-21 04:11:52 +0900

    fix: No.40の実績が正常に解除されない問題を修正

--- src/main/java/com/jaoafa/MyMaid3/Event/Event_AFK.java
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_AFK.java
@@ -65,10 +65,7 @@ public class Event_AFK extends MyMaidLibrary implements Listener {
 			return;
 		}
 		ItemStack is = player.getInventory().getHelmet();
-		if (is == null) {
-			return;
-		}
-		if (is.getType() == Material.ICE) {
+		if (is != null && is.getType() == Material.ICE) {
 			return;
 		}
 
