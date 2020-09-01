f0cf5e193007bac3a24117125fe6721420f36410
Tomachi (ICHIGO) <lego@aol.jp>
2020-09-01 20:06:24 +0900

    change: パケット大量受信で蹴った場合、爆新地内鯖庁外に自動TPするように

--- src/main/java/com/jaoafa/MyMaid3/Event/Event_PacketLimiterKickNotify.java
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_PacketLimiterKickNotify.java
@@ -1,17 +1,16 @@
 package com.jaoafa.MyMaid3.Event;
 
-import java.awt.Color;
-
+import com.jaoafa.MyMaid3.Lib.MyMaidConfig;
+import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
+import net.dv8tion.jda.api.EmbedBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 
-import com.jaoafa.MyMaid3.Lib.MyMaidConfig;
-import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
-
-import net.dv8tion.jda.api.EmbedBuilder;
+import java.awt.*;
+import java.util.Random;
 
 public class Event_PacketLimiterKickNotify extends MyMaidLibrary implements Listener {
 	@EventHandler
@@ -31,7 +30,19 @@ public class Event_PacketLimiterKickNotify extends MyMaidLibrary implements List
 			String location = loc.getWorld().getName() + " " + loc.getBlockX() + " " + loc.getBlockY() + " "
 					+ loc.getBlockZ();
 			embed.addField("座標", location, false);
-			event.getPlayer().teleport(Bukkit.getWorld("Jao_Afa").getSpawnLocation());
+
+			Random rand = new Random();
+			boolean x_isMinus = rand.nextBoolean();
+			int x = rand.nextInt(310) + 152; // 152 - 462
+			x = x_isMinus ? -x : x;
+
+			boolean z_isMinus = rand.nextBoolean();
+			int z = rand.nextInt(310) + 152; // 152 - 462
+			z = x_isMinus ? -z : z;
+
+			Location teleportLoc = new Location(Bukkit.getWorld("Jao_Afa"), x, 70, z);
+			event.getPlayer().teleport(teleportLoc);
+			System.out.println("[PacketLimiter_AutoTP] teleport to Jao_Afa " + x + " 70 " + z);
 
 			MyMaidConfig.getJDA().getTextChannelById(597423444501463040L).sendMessage(embed.build()).queue();
 		}
