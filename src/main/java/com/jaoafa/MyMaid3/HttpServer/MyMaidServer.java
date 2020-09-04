5fd362a3ec988a72684e761a97338037a8a2b495
Tomachi (ICHIGO) <lego@aol.jp>
2020-09-05 06:32:34 +0900

    change: root(/)も受けるように

--- src/main/java/com/jaoafa/MyMaid3/HttpServer/MyMaidServer.java
+++ src/main/java/com/jaoafa/MyMaid3/HttpServer/MyMaidServer.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpServer;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class MyMaidServer extends BukkitRunnable {
@@ -12,6 +13,13 @@ public class MyMaidServer extends BukkitRunnable {
         int port = 31001;
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/", exc -> {
+                final String response = "{\"status\":true,\"message\":\"Hello world.\"}";
+                exc.sendResponseHeaders(200, response.length());
+                OutputStream os = exc.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            });
             server.createContext("/votefill", new Http_VoteFill());
             server.start();
         } catch (IOException e) {
