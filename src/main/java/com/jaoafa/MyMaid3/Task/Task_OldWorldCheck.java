eded0b3b88cd9e7efcda2af500faef8ad3985523
Tomachi (ICHIGO) <lego@aol.jp>
2020-09-17 21:40:56 +0900

    add: 旧ワールドの自動アンロード追加

---
+++ src/main/java/com/jaoafa/MyMaid3/Task/Task_OldWorldCheck.java
@@ -0,0 +1,30 @@
+package com.jaoafa.MyMaid3.Task;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task_OldWorldCheck extends BukkitRunnable {
+    List<String> targetWorlds = Arrays.asList("kassi-hp-tk", "Jao_Afa_1", "Jao_Afa_2", "SandBox_1", "SandBox_2", "SandBox_3", "ReJao_Afa", "Summer2017", "Summer2018");
+
+    @Override
+    public void run() {
+        for (World world : Bukkit.getWorlds()) {
+            boolean isEmpty = targetWorlds.stream().noneMatch(wname -> world.getName().equalsIgnoreCase(wname));
+            if (isEmpty) {
+                continue;
+            }
+
+            if (!world.getPlayers().isEmpty()) {
+                continue;
+            }
+
+            Bukkit.broadcastMessage("[OldWorldCheck] " + ChatColor.GREEN + "ワールド「" + world.getName() + "」にはプレイヤーが誰もいないため、アンロードします。");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mv remove " + world.getName());
+        }
+    }
+}
