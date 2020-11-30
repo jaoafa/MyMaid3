b99d9b88aac81a91a1f5bca2bd12fc12b50a3a55
Tomachi [ICHIGO] <lego@aol.jp>
2020-11-30 20:25:33 +0900

    add: loginコマンド規制

---
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_AntiLoginCmd.java
@@ -0,0 +1,35 @@
+package com.jaoafa.MyMaid3.Event;
+
+import com.jaoafa.MyMaid3.Lib.EBan;
+import com.jaoafa.MyMaid3.Lib.MyMaidConfig;
+import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class Event_AntiLoginCmd extends MyMaidLibrary implements Listener {
+    @EventHandler
+    public void onOPCommand(PlayerCommandPreprocessEvent event) {
+        String command = event.getMessage();
+        Player player = event.getPlayer();
+        String[] args = command.split(" ", 0);
+        if (args.length == 0) {
+            return; // 本来発生しないと思うけど
+        }
+        if (!args[0].equalsIgnoreCase("/login")) {
+            return; // loginコマンド以外
+        }
+
+        EBan eban = new EBan(player);
+        if (eban.isBanned()) {
+            event.setCancelled(true);
+            return;
+        }
+
+        eban.addBan("jaotan", String.format("コマンド「%s」を実行したことにより、サーバルールへの違反の可能性を検知したため", command));
+        player.kickPlayer("Disconnected.");
+        MyMaidConfig.getJaotanChannel().sendMessage(String.format("プレイヤー「%s」がコマンド「%s」を実行したため、キックしました。", player.getName(), command)).queue();
+        event.setCancelled(true);
+    }
+}
