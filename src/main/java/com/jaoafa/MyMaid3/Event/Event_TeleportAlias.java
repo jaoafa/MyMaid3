b32ad75a94a71d99838d701df1e60047b38e3efe
Tomachi [ICHIGO] <lego@aol.jp>
2021-01-09 15:30:50 +0900

    fix: tpaliasでのtpをした際、tpdenyやスペクテイターチェックを回避してしまう問題を修正

--- src/main/java/com/jaoafa/MyMaid3/Event/Event_TeleportAlias.java
+++ src/main/java/com/jaoafa/MyMaid3/Event/Event_TeleportAlias.java
@@ -1,11 +1,10 @@
 package com.jaoafa.MyMaid3.Event;
 
-import com.jaoafa.MyMaid3.Lib.EBan;
-import com.jaoafa.MyMaid3.Lib.Jail;
-import com.jaoafa.MyMaid3.Lib.MyMaidLibrary;
-import com.jaoafa.MyMaid3.Lib.TeleportAlias;
+import com.jaoafa.MyMaid3.Lib.*;
 import com.jaoafa.MyMaid3.Main;
 import com.jaoafa.MyMaid3.Task.Task_TeleportAlias;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -37,6 +36,24 @@ public class Event_TeleportAlias extends MyMaidLibrary implements Listener {
             String replacement = TeleportAlias.getReplaceAlias(to);
             if (replacement == null) {
                 return;
+            }
+            Player replacementPlayer = Bukkit.getPlayer(replacement);
+            if (replacementPlayer != null && replacementPlayer.isOnline()) {
+                if (replacementPlayer.getGameMode() == GameMode.SPECTATOR) {
+                    if (isAMR(player)) {
+                        SendMessage(player, "tp", "プレイヤー「" + replacementPlayer.getName() + "」はスペクテイターモードのためテレポートできません。");
+                    }
+                    event.setCancelled(true);
+                    return;
+                }
+                TpDeny tpDeny = new TpDeny(replacementPlayer);
+                if (tpDeny.isTpDeny(player) || tpDeny.isTpDeny(player)) {
+                    // denied
+                    SendMessage(player, "tpDeny", "指定されたプレイヤー「" + replacementPlayer.getName() + "」へのテレポートは拒否されました。");
+                    if (tpDeny.isNotify(player))
+                        SendMessage(replacementPlayer, "tpDeny", "プレイヤー「" + player.getName() + "」からのテレポートを拒否しました。");
+                    event.setCancelled(true);
+                }
             }
             new Task_TeleportAlias(player, replacement).runTaskLater(Main.getJavaPlugin(), 5L);
             event.setCancelled(true);
